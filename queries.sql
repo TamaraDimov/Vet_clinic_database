9f48da12dfe356420644c49c8049090100d2ae49
TamaraDimov <tamemojsoska@gmail.com>
2023-07-10 16:17:08 +0200

    update queries.sql

--- queries.sql
+++ queries.sql
@@ -10,3 +10,40 @@ SELECT * from animals WHERE name !='Gabumon';
 
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
 
+BEGIN;
+UPDATE animals
+SET species = 'unspecified';
+SELECT * FROM animals;
+ROOLBACK;
+
+BEGIN;
+UPDATE animals
+SET species = 'digimon'
+WHERE name LIKE '%mon';
+
+UPDATE animals
+SET species = 'pokemon'
+WHERE species IS NULL;
+
+COMMIT;
+
+BEGIN;
+DELETE FROM animals;
+SELECT * FROM animals;
+ROOLBACK;
+SELECT FROM animals;
+
+BEGIN;
+DELETE FROM animals;
+WHERE date_of_birth > '2022-01-01';
+SAVEPOINT SP1;
+
+UPDATE animals
+SET weight_kg = weight_kg*-1;
+
+ROOLBACK TO SP1;
+
+UPDATE animals
+SET weight_kg = weight_kg*-1 WHERE weight_kg < 0; 
+
+COMMIT;
