7b974dfe3160d1aadd3ed5d1735ced96c0609643
TamaraDimov <tamemojsoska@gmail.com>
2023-07-11 06:40:46 +0200

    Write queries to answer questions

--- queries.sql
+++ queries.sql
@@ -47,3 +47,15 @@ UPDATE animals
 SET weight_kg = weight_kg*-1 WHERE weight_kg < 0; 
 
 COMMIT;
+
+BEGIN;
+
+SELECT COUNT (id) FROM animals;
+SELECT COUNT(escape_attempts) FROM animals WHERE escape_attempts = 0;
+SELECT AVG (weight_kg) FROM animals;
+SELECT species, MIN(weight_kg), MAX(weight_kg)
+FROM animals
+GROUP BY species;
+SELECT species, AVG(escape_attempts)
+FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND  '2000-12-31'
+GROUP BY species;
